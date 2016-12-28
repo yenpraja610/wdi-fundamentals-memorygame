09ea68f39aefdf47af1dbdf03972b3db835d0341
Yenifer Prajapati <yen.a@me.com>
2016-12-28 04:34:20 -0500

    added if else

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,10 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardOne === cardThree)
+{
+	alert("You found me!");
+} else {
+	alert ("Nope, not me!");
+}
